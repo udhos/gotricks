220d5205cc7fb9ec82df72640d2f9c567f487c95
udhos <udhos>
2022-04-03 01:02:41 -0300

    Comments.

--- tricks/interface01/main.go
+++ tricks/interface01/main.go
@@ -17,7 +17,12 @@ func main() {
 	fmt.Printf("iface1 == nil: %t\n", iface1 == nil)
 
 	// an interface is either nil or has an underlying object
-	// the underlying object can be a nil pointer to a type
+	//   the underlying object has a type and a value
+	//     in case of a pointer type, it can be nil
+	//     HOWEVER a nil interface is different than an interface holding a nil pointer!
+	//     example:
+	//       type:  *bytes.Buffer
+	//       value: nil
 
 	fmt.Println()
 	fmt.Println("interface holding nil pointer to *bytes.Buffer:")
@@ -29,4 +34,11 @@ func main() {
 	fmt.Printf("iface1 is NOT nil:              type=%[1]T value=%[1]v\n", iface1)
 	fmt.Printf("iface1 == nil:                  %t\n", iface1 == nil)
 	fmt.Printf("iface1 == (*bytes.Buffer)(nil): %t\n", iface1 == (*bytes.Buffer)(nil))
+
+	fmt.Println()
+	fmt.Println("let's take a look at an interface holding a non-pointer object:")
+	fmt.Println()
+
+	var iface2 interface{} = [...]int{1, 2}
+	fmt.Printf("iface2: type=%[1]T value=%[1]v\n", iface2)
 }
